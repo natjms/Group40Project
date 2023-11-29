48309bf4789d526624122c76601f92f432a044a9
njms <natjms@njms.ca>
2023-11-29 15:24:51 -0800

    Add map search activity

---
+++ app/src/main/java/group/fortytwo/ratemylandlord/FindPropertyOnMap.java
@@ -0,0 +1,14 @@
+package group.fortytwo.ratemylandlord;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindPropertyOnMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_property_on_map);
+    }
+}
