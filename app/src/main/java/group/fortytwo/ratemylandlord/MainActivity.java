1bc0c943a47b96d64e33a36adc5d43cc7678bd8f
njms <natjms@njms.ca>
2023-12-01 16:24:35 -0800

    Fix task 2 button in MainActivity

--- app/src/main/java/group/fortytwo/ratemylandlord/MainActivity.java
+++ app/src/main/java/group/fortytwo/ratemylandlord/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         MainActivity content = this;
 
         button_task1 = (Button) findViewById(R.id.button_task1);
-        button_task1 = (Button) findViewById(R.id.button_task1);
+        button_task2 = (Button) findViewById(R.id.button_task2);
         View.OnClickListener start_property_search = view -> {
             Intent intent = new Intent(this, PropertySearch.class);
             startActivity(intent);
@@ -26,6 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
         // Tasks 1 and 2 start from the same activity
         button_task1.setOnClickListener(start_property_search);
-        button_task1.setOnClickListener(start_property_search);
+        button_task2.setOnClickListener(start_property_search);
     }
 }
