02a46be3ef987ed94150265695bab00990c5083f
notbaopham <bpham03@student.ubc.ca>
2023-12-04 16:02:33 -0800

    Added Files for Leaving Review and Verification (Verification is still in progress)

---
+++ app/src/main/java/group/fortytwo/ratemylandlord/LeavesReview.java
@@ -0,0 +1,14 @@
+package group.fortytwo.ratemylandlord;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LeavesReview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaves_review);
+    }
+}
