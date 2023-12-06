5660bdf2a83fb3f175bf527e1a65fa0113f02b02
njms <natjms@njms.ca>
2023-12-05 18:41:16 -0800

    Add progress indicator to Verification3

--- app/src/main/java/group/fortytwo/ratemylandlord/Verification3.java
+++ app/src/main/java/group/fortytwo/ratemylandlord/Verification3.java
@@ -7,22 +7,46 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class Verification3 extends AppCompatActivity {
     CheckBox check_reject;
+    TextView text_status;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification3);
 
         check_reject = (CheckBox) findViewById(R.id.check_reject);
+        text_status = (TextView) findViewById(R.id.text_status);
 
         final Handler handler = new Handler(Looper.getMainLooper());
+
+        handler.postDelayed(() ->
+                text_status.setText("Your documents have been recieved"),
+                2000
+        );
+
+        handler.postDelayed(() ->
+                        text_status.setText("The team is reviewing your documents"),
+                4000
+        );
+
+        handler.postDelayed(() -> {
+                if (check_reject.isChecked()) {
+                    text_status.setText("Verifying the accuracy of your documents...");
+                } else {
+                    text_status.setText("Your application is being processed");
+                }
+            },
+            8000
+        );
+
         handler.postDelayed(() -> {
             Intent intent = new Intent(this, Verification4.class);
             intent.putExtra("reject", check_reject.isChecked());
             startActivity(intent);
             finish();
-        }, 10000);
+        }, 12000);
     }
 }
